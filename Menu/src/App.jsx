5e0f4b9c6b6970ae0286d42d366da1b038c41d34
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-09 21:42:39 +0200

    refactor: extract HeaderMenu component and import it in App.jsx

--- Menu/src/App.jsx
+++ Menu/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import HeaderMenu  from "./components/HeaderMenu";
 
 function App() {
   return (
     <div>
-      <HeaderMenu />
+      <HeaderMenu/>
       <MainMenu />
       <FooterMenu />
     </div>
@@ -12,20 +13,7 @@ function App() {
 
 export default App;
 
-function HeaderMenu() {
-  return (
-    <div>
-      <h1 className="text-5xl text-center font-extrabold text-stone-700 my-10">
-        {" "}
-        Welcome to <span className="text-red-950">"PS"</span> Pizzeria
-      </h1>
-      <p className="text-center text-3xl font-medium text-stone-700">
-        {" "}
-        here you can find your favorite pizza and then order it ;){" "}
-      </p>
-    </div>
-  );
-}
+
 
 function MainMenu() {
   return (
---
+++ Menu/src/components/HeaderMenu.jsx
@@ -0,0 +1,15 @@
+
+export default function HeaderMenu (){
+    return(
+        <div>
+      <h1 className="text-5xl text-center font-extrabold text-stone-700 my-10">
+        {" "}
+        Welcome to <span className="text-red-950">"PS"</span> Pizzeria
+      </h1>
+      <p className="text-center text-3xl font-medium text-stone-700">
+        {" "}
+        here you can find your favorite pizza and then order it ;){" "}
+      </p>
+    </div>
+    )
+}
