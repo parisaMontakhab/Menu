9a72d8309ac6ba61e7682ee78eec9f204ea01607
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-09 21:49:31 +0200

    refactor: extract and import FooterMenu in App.jsx

--- Menu/src/App.jsx
+++ Menu/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import FooterMenu from "./components/FooterMenu";
 import HeaderMenu  from "./components/HeaderMenu";
 import MainMenu from "./components/MainMenu";
 
@@ -7,7 +8,7 @@ function App() {
     <div>
       <HeaderMenu/>
       <MainMenu/>
-      <FooterMenu />
+      <FooterMenu/>
     </div>
   );
 }
@@ -20,20 +21,3 @@ export default App;
 
 
 
-function FooterMenu() {
-  return (
-    <div className="text-center my-20">
-      <h2 className="text-3xl font-medium text-stone-700 ">
-        {" "}
-        we're open untill 22 come visit us or order online !
-      </h2>
-      <button
-        className=" transition  ease-in-out delay-150 w-40 bg-red-300 p-4 rounded-3xl mt-10 text-stone-600 font-semibold text-xl drop-shadow-md 
-      hover:bg-red-400 hover:text-stone-700 hover:-translate  duration-300 "
-      >
-        {" "}
-        order{" "}
-      </button>
-    </div>
-  );
-}
---
+++ Menu/src/components/FooterMenu.jsx
@@ -0,0 +1,18 @@
+
+export default function FooterMenu (){
+    return(
+        <div className="text-center my-20">
+        <h2 className="text-3xl font-medium text-stone-700 ">
+          {" "}
+          we're open untill 22 come visit us or order online !
+        </h2>
+        <button
+          className=" transition  ease-in-out delay-150 w-40 bg-red-300 p-4 rounded-3xl mt-10 text-stone-600 font-semibold text-xl drop-shadow-md 
+        hover:bg-red-400 hover:text-stone-700 hover:-translate  duration-300 "
+        >
+          {" "}
+          order{" "}
+        </button>
+      </div>
+    )
+}
