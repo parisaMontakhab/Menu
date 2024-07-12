2e5310529e4a8a3828b3a6223486a0a1d016b72c
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-12 18:31:23 +0200

    debug: debug about time section

--- Menu/src/components/FooterMenu.jsx
+++ Menu/src/components/FooterMenu.jsx
@@ -1,16 +1,18 @@
 
 export default function FooterMenu (){
-  const nowTime = new Date().toLocaleTimeString();
-  const openTime = 11;
-  const closeTime = 22;
+ const hour = new Date().getHours();
+ const openHour = 11;
+ const closeHour = 22;
+ const isOpen = hour >= openHour && hour<= closeHour ;
+  
     return(
         <div className="text-center my-20">
         <h2 className="text-3xl font-medium text-stone-700 ">
           {" "}
           we're open untill 22 come visit us or order online !
         </h2>
-        <p className=" text-stone-500 font-medium mt-2">the time is {nowTime} now! 
-         <p>{nowTime>openTime && nowTime<closeTime ? 'We are open :)' : 'we are close :('}</p> 
+        <p className=" text-stone-500 font-medium mt-2">the time is {hour} now! 
+         <p>{ isOpen  ? 'We are open :)' : 'we are close :('}</p> 
         </p>
         <button
           className=" transition  ease-in-out delay-150 w-40 bg-red-300 p-4 rounded-3xl mt-10 text-stone-600 font-semibold text-xl drop-shadow-md 
