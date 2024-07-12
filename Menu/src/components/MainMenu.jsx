1ffd25d89a19ea909dd63d7cc843a3f1df142588
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-12 18:51:56 +0200

    refactor : changed about style and add soldout

--- Menu/src/components/MainMenu.jsx
+++ Menu/src/components/MainMenu.jsx
@@ -5,6 +5,7 @@ const pizzaList = [
     alt: "Pizza Fochaccia",
     des: " Pizza Focaccia is typically made with a base of olive oil, garlic, rosemary, and sea salt, often topped with tomatoes and other fresh ingredients.",
     price: 10,
+    soldOut : false,
   },
   {
     id: 2,
@@ -12,6 +13,7 @@ const pizzaList = [
     alt: "Pizza Formaggio",
     des: " Pizza Formaggio is a delightful blend of multiple cheeses, creating a rich and creamy experience with every bite.",
     price: 15,
+    soldOut : true,
   },
   {
     id: 3,
@@ -19,6 +21,7 @@ const pizzaList = [
     alt: "Pizza Funghi",
     des: " Pizza Funghi is a savory delight featuring a generous topping of mushrooms, offering a deliciously earthy and umami-rich flavor.",
     price: 12,
+    soldOut : false,
   },
   {
     id: 4,
@@ -26,6 +29,7 @@ const pizzaList = [
     alt: "Pizza Margarita",
     des: " Pizza Margherita is a classic Italian dish topped with fresh tomatoes, mozzarella cheese, and basil, representing the colors of the Italian flag.",
     price: 9,
+    soldOut : true,
   },
   {
     id: 5,
@@ -33,6 +37,7 @@ const pizzaList = [
     alt: "Pizza Spaghetti",
     des: " Pizza Spaghetti is a unique fusion dish that combines the flavors of pizza and spaghetti, featuring a crust topped with spaghetti, marinara sauce, and melted cheese.",
     price: 12,
+    soldOut : false,
   },
   {
     id: 6,
@@ -40,6 +45,7 @@ const pizzaList = [
     alt: "Pizza Spinaci",
     des: " Pizza Spinaci is a nutritious and flavorful choice, topped with fresh spinach, garlic, mozzarella cheese, and often a hint of ricotta or feta, offering a delightful mix of creamy and savory tastes.",
     price: 13,
+    soldOut : false,
   },
 ];
 
@@ -52,22 +58,23 @@ export default function MainMenu() {
             <img
               src={Pizza.img}
               alt={Pizza.alt}
-              className="rounded-full bg-stone-500 shadow-lg shadow-stone-500/100 w-56 h-64"
+              className={Pizza.soldOut ? "rounded-full bg-stone-500 shadow-lg shadow-stone-500/100 w-56 h-64 opacity-50": "rounded-full bg-stone-500 shadow-lg shadow-stone-500/100 w-56 h-64"}
             />
           </div>
-          <div className="mt-14 w-64 text-pretty text-stone-500">
+          <div className={Pizza.soldOut ? 'mt-14 w-64 text-pretty text-stone-400' : 'mt-14 w-64 text-pretty text-stone-700'}>
             <p className="font-bold">
                 {Pizza.alt}
             </p>
             <p>
              {Pizza.des}
             </p>
-            <p className="mt-5 font-bold"> {Pizza.price} $</p>
+            <p className="mt-5 font-bold"> {Pizza.soldOut ? 'sold Out' : Pizza.price +' $'} </p>
           </div>
         </li>
       ))}
     </ul>
   );
 }
+
 
 
